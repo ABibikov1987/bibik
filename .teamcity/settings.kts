b12320a5923555724e17a234736ea68b6d73bea1
admin <admin@TeamCity>
2021-03-11 18:29:35 +0300

    TeamCity change in 'Bibik' project: VCS roots of 'Build' build configuration were updated

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -57,6 +57,10 @@ object Bibik : Project({
 
 object Bibik_Build : BuildType({
     name = "Build"
+
+    vcs {
+        root(DslContext.settingsRoot)
+    }
 })
 
 object Bibik_HttpsABibikov1987githubComABibikov1987bibikRefsHeadsMain : GitVcsRoot({
