3494d24cc128a8ccc538968499cc40bb7fe68265
ABibikov1987 <bibikeych@mail.ru>
2021-03-12 12:01:49 +0300

    777

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,101 +1,8 @@
+package Test.buildTypes
+
 import jetbrains.buildServer.configs.kotlin.v2019_2.*
-import jetbrains.buildServer.configs.kotlin.v2019_2.projectFeatures.buildReportTab
-import jetbrains.buildServer.configs.kotlin.v2019_2.vcs.GitVcsRoot
 import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.script
 import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.finishBuildTrigger
-
-/*
-The settings script is an entry point for defining a TeamCity
-project hierarchy. The script should contain a single call to the
-project() function with a Project instance or an init function as
-an argument.
-
-<<<<<<< HEAD
-/*
-The settings script is an entry point for defining a TeamCity
-project hierarchy. The script should contain a single call to the
-project() function with a Project instance or an init function as
-an argument.
-
-VcsRoots, BuildTypes, Templates, and subprojects can be
-registered inside the project using the vcsRoot(), buildType(),
-template(), and subProject() methods respectively.
-
-To debug settings scripts in command-line, run the
-
-    mvnDebug org.jetbrains.teamcity:teamcity-configs-maven-plugin:generate
-
-command and attach your debugger to the port 8000.
-
-To debug in IntelliJ Idea, open the 'Maven Projects' tool window (View
--> Tool Windows -> Maven Projects), find the generate task node
-(Plugins -> teamcity-configs -> teamcity-configs:generate), the
-'Debug' option is available in the context menu for the task.
-*/
-
-version = "2020.2"
-
-project {
-    description = "Contains all other projects"
-
-=======
-VcsRoots, BuildTypes, Templates, and subprojects can be
-registered inside the project using the vcsRoot(), buildType(),
-template(), and subProject() methods respectively.
-
-To debug settings scripts in command-line, run the
-
-    mvnDebug org.jetbrains.teamcity:teamcity-configs-maven-plugin:generate
-
-command and attach your debugger to the port 8000.
-
-To debug in IntelliJ Idea, open the 'Maven Projects' tool window (View
--> Tool Windows -> Maven Projects), find the generate task node
-(Plugins -> teamcity-configs -> teamcity-configs:generate), the
-'Debug' option is available in the context menu for the task.
-*/
-
-version = "2020.2"
-
-project {
-    description = "Contains all other projects"
-
->>>>>>> 38bd88ea27d29962bcf60695644a558e933e31e4
-    features {
-        buildReportTab {
-            id = "PROJECT_EXT_1"
-            title = "Code Coverage"
-            startPage = "coverage.zip!index.html"
-        }
-    }
-
-    cleanup {
-        baseRule {
-            preventDependencyCleanup = false
-        }
-    }
-
-    subProject(Bibik)
-}
-
-
-object Bibik : Project({
-    name = "Bibik"
-
-    vcsRoot(Bibik_HttpsGithubComABibikov1987bibikRefsHeadsMain)
-})
-
-object Bibik_HttpsGithubComABibikov1987bibikRefsHeadsMain : GitVcsRoot({
-    name = "https://github.com/ABibikov1987/bibik#refs/heads/main"
-    url = "https://github.com/ABibikov1987/bibik"
-    branch = "refs/heads/main"
-    branchSpec = "refs/heads/*"
-    authMethod = password {
-        userName = "ABibikov1987"
-        password = "credentialsJSON:5297c011-7710-4aac-81f0-88e88e00a9bc"
-<<<<<<< HEAD
-    }
-})
 
 
 object Test_Test : BuildType({
@@ -140,7 +47,5 @@ object Test_Test2 : BuildType({
         snapshot(Test_Test) {
             onDependencyFailure = FailureAction.FAIL_TO_START
         }
-=======
->>>>>>> 38bd88ea27d29962bcf60695644a558e933e31e4
     }
 })
