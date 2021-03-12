727877d58741a3c62a31887592be9b2038ca0522
admin <admin@TeamCity>
2021-03-12 11:48:10 +0300

    TeamCity change in project with id Test: project 'Test' was removed

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,7 +1,5 @@
 import jetbrains.buildServer.configs.kotlin.v2019_2.*
-import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.script
 import jetbrains.buildServer.configs.kotlin.v2019_2.projectFeatures.buildReportTab
-import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.finishBuildTrigger
 
 /*
 The settings script is an entry point for defining a TeamCity
@@ -43,59 +41,4 @@ project {
             preventDependencyCleanup = false
         }
     }
-
-    subProject(Test)
 }
-
-
-object Test : Project({
-    name = "Test"
-    description = "тестовый проект"
-
-    buildType(Test_Test)
-    buildType(Test_Test2)
-})
-
-object Test_Test : BuildType({
-    name = "test"
-
-    steps {
-        script {
-            name = "step1"
-            scriptContent = "echo step1"
-        }
-        script {
-            name = "step2"
-            scriptContent = "echo step2"
-        }
-    }
-})
-
-object Test_Test2 : BuildType({
-    name = "test2"
-    description = "bild2"
-
-    steps {
-        script {
-            name = "step1"
-            scriptContent = "echo step3"
-        }
-        script {
-            name = "step4"
-            scriptContent = "echo step4"
-        }
-    }
-
-    triggers {
-        finishBuildTrigger {
-            buildType = "${Test_Test.id}"
-            successfulOnly = true
-        }
-    }
-
-    dependencies {
-        snapshot(Test_Test) {
-            onDependencyFailure = FailureAction.FAIL_TO_START
-        }
-    }
-})
