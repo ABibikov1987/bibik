38bd88ea27d29962bcf60695644a558e933e31e4
admin <admin@TeamCity>
2021-03-12 11:51:33 +0300

    TeamCity change in 'Bibik' project: project was created

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,5 +1,6 @@
 import jetbrains.buildServer.configs.kotlin.v2019_2.*
 import jetbrains.buildServer.configs.kotlin.v2019_2.projectFeatures.buildReportTab
+import jetbrains.buildServer.configs.kotlin.v2019_2.vcs.GitVcsRoot
 
 /*
 The settings script is an entry point for defining a TeamCity
@@ -41,4 +42,24 @@ project {
             preventDependencyCleanup = false
         }
     }
+
+    subProject(Bibik)
 }
+
+
+object Bibik : Project({
+    name = "Bibik"
+
+    vcsRoot(Bibik_HttpsGithubComABibikov1987bibikRefsHeadsMain)
+})
+
+object Bibik_HttpsGithubComABibikov1987bibikRefsHeadsMain : GitVcsRoot({
+    name = "https://github.com/ABibikov1987/bibik#refs/heads/main"
+    url = "https://github.com/ABibikov1987/bibik"
+    branch = "refs/heads/main"
+    branchSpec = "refs/heads/*"
+    authMethod = password {
+        userName = "ABibikov1987"
+        password = "credentialsJSON:5297c011-7710-4aac-81f0-88e88e00a9bc"
+    }
+})
