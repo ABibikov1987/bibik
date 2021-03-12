b3004a577218f7882e077dc8206e541a3927339a
ABibikov1987 <bibikeych@mail.ru>
2021-03-12 11:31:31 +0300

    777

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -2,27 +2,11 @@ package Test.buildTypes
 
 import jetbrains.buildServer.configs.kotlin.v2019_2.*
 import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.script
-<<<<<<< HEAD
+
 import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.finishBuildTrigger
-=======
 import jetbrains.buildServer.configs.kotlin.v2019_2.projectFeatures.buildReportTab
-import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.finishBuildTrigger
 import jetbrains.buildServer.configs.kotlin.v2019_2.vcs.GitVcsRoot
 
-/*
-The settings script is an entry point for defining a TeamCity
-project hierarchy. The script should contain a single call to the
-project() function with a Project instance or an init function as
-an argument.
-
-VcsRoots, BuildTypes, Templates, and subprojects can be
-registered inside the project using the vcsRoot(), buildType(),
-template(), and subProject() methods respectively.
-
-To debug settings scripts in command-line, run the
-
-    mvnDebug org.jetbrains.teamcity:teamcity-configs-maven-plugin:generate
->>>>>>> 16cb27d8891396f99c1c9ae6df047c4ad022e740
 
 
 version = "2020.2"
@@ -32,8 +16,7 @@ project {
     buildType(Test_Test2)
 
         }
-<<<<<<< HEAD
-=======
+
     }
 
     subProject(Test)
@@ -41,7 +24,6 @@ project {
     subProject(Bibik)
 }
 
->>>>>>> 16cb27d8891396f99c1c9ae6df047c4ad022e740
 
 object Test_Test : BuildType({
     name = "test"
@@ -59,14 +41,11 @@ object Test_Test : BuildType({
 })
 
 
-<<<<<<< HEAD
-<<<<<<< HEAD
 object Test_Test2 : BuildType({
     name = "test2"
     description = "bild2"
 
-=======
-=======
+
 object Finish : Project({
     name = "finish"
 
@@ -84,7 +63,6 @@ object Finish_HttpsGithubComABibikov1987bibik : GitVcsRoot({
 })
 
 
->>>>>>> 944ec5d44725889f924ae6d8129063989d2c15ee
 object Test : Project({
     name = "Test"
     description = "тестовый проект"
@@ -96,22 +74,19 @@ object Test : Project({
 object Test_Test : BuildType({
     name = "test"
 
-<<<<<<< HEAD
->>>>>>> 16cb27d8891396f99c1c9ae6df047c4ad022e740
     vcs {
         root(DslContext.settingsRoot)
     }
 
-=======
->>>>>>> 944ec5d44725889f924ae6d8129063989d2c15ee
+
     steps {
         script {
             name = "step1"
-<<<<<<< HEAD
+
             scriptContent = "echo step3"
         }
         script {
-=======
+
             scriptContent = "echo step1"
         }
         script {
@@ -131,7 +106,7 @@ object Test_Test2 : BuildType({
             scriptContent = "echo step3"
         }
         script {
->>>>>>> 16cb27d8891396f99c1c9ae6df047c4ad022e740
+
             name = "step4"
             scriptContent = "echo step4"
         }
