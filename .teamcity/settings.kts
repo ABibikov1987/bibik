3355fa409bcf78c6eb95d515fc76e4158f4068d6
admin <admin@TeamCity>
2021-03-12 11:46:53 +0300

    TeamCity change in 'Bibik' project: 'Build' build configuration was removed

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,22 +1,48 @@
-package Test.buildTypes
-
 import jetbrains.buildServer.configs.kotlin.v2019_2.*
 import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.script
-
-import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.finishBuildTrigger
 import jetbrains.buildServer.configs.kotlin.v2019_2.projectFeatures.buildReportTab
+import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.finishBuildTrigger
 import jetbrains.buildServer.configs.kotlin.v2019_2.vcs.GitVcsRoot
 
+/*
+The settings script is an entry point for defining a TeamCity
+project hierarchy. The script should contain a single call to the
+project() function with a Project instance or an init function as
+an argument.
 
+VcsRoots, BuildTypes, Templates, and subprojects can be
+registered inside the project using the vcsRoot(), buildType(),
+template(), and subProject() methods respectively.
+
+To debug settings scripts in command-line, run the
+
+    mvnDebug org.jetbrains.teamcity:teamcity-configs-maven-plugin:generate
+
+command and attach your debugger to the port 8000.
+
+To debug in IntelliJ Idea, open the 'Maven Projects' tool window (View
+-> Tool Windows -> Maven Projects), find the generate task node
+(Plugins -> teamcity-configs -> teamcity-configs:generate), the
+'Debug' option is available in the context menu for the task.
+*/
 
 version = "2020.2"
+
 project {
-    
-    buildType(Test_Test)
-    buildType(Test_Test2)
+    description = "Contains all other projects"
 
+    features {
+        buildReportTab {
+            id = "PROJECT_EXT_1"
+            title = "Code Coverage"
+            startPage = "coverage.zip!index.html"
         }
+    }
 
+    cleanup {
+        baseRule {
+            preventDependencyCleanup = false
+        }
     }
 
     subProject(Test)
@@ -25,25 +51,9 @@ project {
 }
 
 
-object Test_Test : BuildType({
-    name = "test"
-
-    steps {
-        script {
-            name = "step1"
-            scriptContent = "echo шаг первый"
-        }
-        script {
-            name = "step2"
-            scriptContent = "echo шаг второй"
-        }
-    }
+object Bibik : Project({
+    name = "Bibik"
 })
-
-
-object Test_Test2 : BuildType({
-    name = "test2"
-    description = "bild2"
 
 
 object Finish : Project({
@@ -74,19 +84,9 @@ object Test : Project({
 object Test_Test : BuildType({
     name = "test"
 
-    vcs {
-        root(DslContext.settingsRoot)
-    }
-
-
     steps {
         script {
             name = "step1"
-
-            scriptContent = "echo step3"
-        }
-        script {
-
             scriptContent = "echo step1"
         }
         script {
@@ -103,12 +103,11 @@ object Test_Test2 : BuildType({
     steps {
         script {
             name = "step1"
-            scriptContent = "echo шаг отработал"
+            scriptContent = "echo step3"
         }
         script {
-
             name = "step4"
-            scriptContent = "echo шаг блок 2 отработал по триггеру и образу"
+            scriptContent = "echo step4"
         }
     }
 
