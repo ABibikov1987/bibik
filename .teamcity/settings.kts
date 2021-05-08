94f60efa35f11e2a94febc904ef4129eeb907770
admin <admin@TeamCity>
2021-05-08 12:55:20 +0300

    TeamCity change in 'python' project: parameters of 'python_pipline' build configuration were updated

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -74,10 +74,10 @@ object Python_PythonPipline : BuildType({
     params {
         select("stend", "", label = "stends", display = ParameterDisplay.PROMPT,
                 options = listOf("K3", "K4", "NT", "IFT", "PSI", "PROD"))
+        select("namespase", "", label = "choose namespase", display = ParameterDisplay.PROMPT,
+                options = listOf("a", "b"))
         select("K4", "", label = "K4", display = ParameterDisplay.PROMPT,
                 options = listOf(""""rb"""", """"kb"""", """"oc""""))
-        select("choose_namespase", "", label = "choose namespase", display = ParameterDisplay.PROMPT,
-                options = listOf("a", "b"))
     }
 
     steps {
